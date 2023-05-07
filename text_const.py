85e8d5e389bc4eb5e53cbec553654e999a5e08cd
Alice Vasileva <lergo2004@gmail.com>
2023-05-07 23:09:10 +0200

    Finished

--- text_const.py
+++ text_const.py
@@ -2,4 +2,7 @@ hello = "Hi there, my name is Lucy. Its a 'Q & A' game where I will ask you some
         "Now we will create our teams."
 
 start_game = "Soooooo...Now I will ask you 10 question. Тот кто быстрее нажмет свою клавишу - будет отвечать. " \
-             "Для {name1} команды клавиша 'a', для {name2} - 'l'"
+             "Для команды '{name1}' клавиша 'a', для '{name2}' - 'l'"
+
+end_game = "Вот и конец игры. Результаты: Команда '{name1}' набрала {score1} очков! А '{name2}' - {score2}." \
+           " Поздравляю!!!"
