497462df23e9cf8ad7d77883f9d88257fd85f58d
Alice Vasileva <lergo2004@gmail.com>
2023-05-09 15:40:03 +0200

    i hope its the last version

--- text_const.py
+++ text_const.py
@@ -6,6 +6,5 @@ start_game = "Soooooo...Now I will ask you 10 question. The one who presses his 
              "For command '{name1}' the key is 'a', for '{name2}' - 'l'"
 
 end_game = "Congratulations on finishing game! I hope you enjoyed testing your knowledge. " \
-           " Thanks for playing, and I hope to see you " \
-           "again soon for more fun and games! Results: Team '{name1}' scored {score1} points!" \
-           " And '{name2}' is {score2}."
+           " Thanks for playing!" \
+           "again soon for more fun and games! Results: '{name1}' won this game!!!!"
